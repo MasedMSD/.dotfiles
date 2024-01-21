1027fc149309c320f0e7fa0fd352d9fd0ef6dfb4
MasedMSD <roblox.hack@mail.ru>
2024-01-21 15:18:32 +0500

    dotfiles

---
+++ winfetch/config.ps1
@@ -0,0 +1,27 @@
+# ===== WINFETCH CONFIGURATION =====
+$logo = "Windows 11"
+$ShowDisks = @("*")
+$ShowPkgs = @("winget")
+$cpustyle = 'bartext'
+$memorystyle = 'bartext'
+$diskstyle = 'bartext'
+
+@(
+    "title"
+    "dashes"
+    "terminal"
+    "pkgs"
+    "pwsh"
+    "dashes"
+    "resolution"
+    "cpu"
+    "gpu"
+    "cpu_usage"
+    "memory"
+    "disk"
+    "dashes"
+    "uptime"
+    "theme"
+    "blank"
+    "colorbar"
+)
